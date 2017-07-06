d9adec4bfb8e972e337e8c359afe853f219353ad
dmelazo <davi@melazo.net>
2017-07-06 17:39:14 -0300

    Exercício do TSP para uso no TP 1

---
+++ Estacionamento.Apresentacao/EstacionamentoForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Estacionamento.Negocio;
+
+namespace Estacionamento.Apresentacao
+{
+    public partial class EstacionamentoForm : Form
+    {
+        public EstacionamentoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string placa = textBox1.Text;
+
+            try
+            {
+
+
+                Operacoes.Checkin(placa);
+
+                MessageBox.Show(String.Format("Placa '{0}' adicionada.", placa));
+                textBox1.Text = string.Empty;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string placa = textBox1.Text;
+
+            try
+            {
+                var valor = Operacoes.Checkout(placa);
+
+                MessageBox.Show(String.Format("Placa '{0}' valor de R${1}.", placa, valor));
+                textBox1.Text = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Estacionamento.Negocio/Operacoes.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento.Negocio
+{
+    /// <summary>
+    /// Classe com as operações de um estacionamento.
+    /// </summary>
+    public sealed class Operacoes
+    {
+        private const int VAGAS_TOTAIS = 15;
+        private static IDictionary<string, DateTime> _estacionamento = new Dictionary<string, DateTime>();
+
+        /// <summary>
+        /// Valida se a operação checkin pode ser realizada
+        /// </summary>
+        private static bool ValidarCheckin(string placa)
+        {
+            if (String.Equals(placa.Trim(), string.Empty))
+                throw new Exception(String.Format("Placa inválida.", placa));
+
+            if (_estacionamento.Count == VAGAS_TOTAIS)
+                throw new Exception("Estacionamento cheio!");
+
+            if (_estacionamento.ContainsKey(placa))
+                throw new Exception(String.Format("Carro placa '{0} já existe!", placa));
+
+            return true;
+        }
+
+        /// <summary>
+        /// Valida se a operação de checkout pode ser realizada
+        /// </summary>
+        private static bool ValidarCheckout(string placa)
+        { 
+            if (String.Equals(placa.Trim(), string.Empty))
+                throw new Exception(String.Format("Placa inválida.", placa));
+
+            if (!_estacionamento.ContainsKey(placa))
+                throw new Exception(String.Format("Carro placa '{0}' NÃO existe!", placa));
+
+            return true;
+        }
+
+        /// <summary>
+        /// Registra a entrada de um carro no estacionamento.
+        /// </summary>
+        public static void Checkin(string placa)
+        {
+            ValidarCheckin(placa);
+            _estacionamento.Add(placa, DateTime.Now);
+            
+        }
+
+        /// <summary>
+        /// Registra a saída de um carro do estacionamento.
+        /// </summary>
+        public static double Checkout(string placa)
+        {
+            ValidarCheckout(placa);
+            DateTime horaEntrada = _estacionamento[placa];
+
+            _estacionamento.Remove(placa);
+
+            return CalcularValorEstacionamento(horaEntrada, DateTime.Now);
+        }
+
+        /// <summary>
+        /// Calcula o valor com base no tempo de permanência
+        /// </summary>
+        private static double CalcularValorEstacionamento(DateTime entrada, DateTime saida)
+        {
+            var permanencia = saida.Subtract(entrada);
+            return Math.Round((permanencia.TotalMinutes / 3), 2); // 3 reais é o valor mínimo
+        }
+
+    }
+}
